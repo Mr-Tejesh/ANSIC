6be9be7888ad4114cd1572592b1c4b116c596918
Mr-Tejesh <114306366+Mr-Tejesh@users.noreply.github.com>
2022-09-27 19:04:38 +0530

    Create Psample.c

---
+++ Chapter2/Psample.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+   printf("hello world! This program is written on train to not loose the lost streak in github. Hope from today.... I won't loose the streak. Writing from Virangaba Lakshmibhai junction");
+   return 0;
+}
