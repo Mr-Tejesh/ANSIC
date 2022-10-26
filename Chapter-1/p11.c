98705d5d13550d8335e5cc2239348fedbbbf7296
Mr-Tejesh <114306366+Mr-Tejesh@users.noreply.github.com>
2022-10-26 16:16:12 +0000

    Delete Chapter-1 directory

--- Chapter-1/p11.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	printf("Tejesh\nD.no:2,KSV\nVZG 323232");
-	return 0;
-}
--- Chapter-1/p12.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	printf("-----------\n|Tejes      |\n|D.no:2,KSV |\n|VZG, 323232|\n-----------");
-	return 0;
-}
