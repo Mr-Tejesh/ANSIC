7626ba93e9c54822ac5d046c38e174bc010af12c
Mr-Tejesh <114306366+Mr-Tejesh@users.noreply.github.com>
2022-10-29 15:06:54 +0000

    Delete Chapter2 directory

--- Chapter2/Psample.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-   printf("hello world! This program is written on train to not loose the lost streak in github. Hope from today.... I won't loose the streak. Writing from Virangaba Lakshmibhai junction");
-   return 0;
-}
--- Chapter2/p25.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	float rice,sugar;
-	printf("\n\n\t\tPlease enter the price of RICE and SUGAR : ");
-	scanf(" %f %f",&rice,&sugar);
-	printf("\n\n\t\t*** LIST OF ITEMS ***\n\t\tItem\t    Price\n\t\tRice\t    Rs %.2f\n\t\tSugar\t    Rs %.2f\n\n",rice,sugar);
-	return 0;
-}
