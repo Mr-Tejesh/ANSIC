8862ee628f95349d44abef0ad4db392ee51db68e
Mr-Tejesh <114306366+Mr-Tejesh@users.noreply.github.com>
2022-10-30 15:51:01 +0000

    Delete Chapter3 directory

--- Chapter3/p34.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	float a,b;
-	printf("\n\n\t\tPlease enter the length and width of the rectangle : ");
-	scanf(" %f %f",&a,&b);
-	printf("\n\n\t\tThe area of rectangle is : %.2f\n\t\tThe perimeter of rectangle is : %.2f",a*b,2*(a+b));
-	return 0;
-}
